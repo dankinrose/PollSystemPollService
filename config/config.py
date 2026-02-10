8da3558ca2eca99049e34e28f24ca1f978c5a113
dankinrose <dankinrose@gmail.com>
2026-02-10 03:41:33 +0200

    DOCK

--- config/config.py
+++ config/config.py
@@ -2,15 +2,10 @@ from pydantic import BaseSettings
 
 
 class Config(BaseSettings):
-    # DB
     MYSQL_USER: str = "user"
     MYSQL_PASSWORD: str = "password"
     MYSQL_DATABASE: str = "poll_service"
-
-    # 👇 חשוב מאוד: שם ה־service ב־docker-compose
     MYSQL_HOST: str = "poll-db"
-
-    # 👇 בתוך Docker תמיד 3306
     MYSQL_PORT: str = "3306"
 
     @property
@@ -20,7 +15,6 @@ class Config(BaseSettings):
             f"@{self.MYSQL_HOST}:{self.MYSQL_PORT}/{self.MYSQL_DATABASE}"
         )
 
-    # Internal API (גם זה בתוך Docker)
     USER_SERVICE_BASE_URL: str = "http://user-service:8000"
 
     class Config:
